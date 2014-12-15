ff4bf20d387319656211d077dcfdfc9eaf09a4db
Faul <faullik@gmail.com>
2014-12-16 00:31:55 +0200

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,41 @@
+Exec { path => "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"}
+
+# Global variables
+$user = "Faul"
+$password = "1234"
+$project = "project"
+
+class apt {
+  exec { "apt-get update" :
+    timeout => 0
+  }
+}
+
+class user {
+  exec { "add user":
+    command => "sudo useradd -m -G sudo -s /bin/bash ${user}",
+    unless => "id -u ${user}"
+  }
+}
+class python {
+  package { "curl":
+    ensure => latest,
+    require => Class["apt"]
+  }
+
+  package { "python":
+    ensure => latest,
+    require => Class["apt"]
+  }
+  package { "python-dev":
+    ensure => latest,
+    require => Class["apt"]
+  }
+}
+
+class software {
+  package { "git":
+    ensure => latest,
+    requre => Class["apt"]
+  }
+}
